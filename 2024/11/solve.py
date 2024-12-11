67846f037e73018e0fc0cefa4b54f71d321c4531
Michal Gorzalczany <michal@gorzalczany.pl>
2024-12-11 08:05:14 +0100

    solve 2024/11

---
+++ 2024/11/solve.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""AOC 11th day."""
+import sys
+from collections import Counter
+
+def splitStone(stone):
+    half = len(stone)//2
+    return stone[:half], str(int(stone[half:]))
+
+def blinkAt(stone):
+    newStones = []
+    if len(stone) == 1 and int(stone) == 0: 
+        newStones.append("1")
+    elif len(stone) % 2 == 0:
+        newStones = splitStone(stone)
+    else:
+         newStones.append(str(int(stone) * 2024))
+    return newStones
+
+def main(input_file):
+    line =  input_file.read()
+    stones = line.split()
+
+    stones_counter = Counter()
+    for stone in stones:
+        stones_counter[stone] += 1
+        
+    for i in range(0, 75):
+        new_counter = Counter()
+        for stone, count in stones_counter.items():
+            stones_counter[stone] = 0
+            newStones = blinkAt(stone)
+            for newStone in newStones:
+                new_counter[newStone] += count
+        stones_counter = stones_counter+new_counter
+        
+    print(sum(stones_counter.values()))
+
+if __name__ == '__main__':
+    env_test_run = sys.argv[-1] == '-t'
+    main(open('01.txt' if not env_test_run else '00.txt', 'r'))
